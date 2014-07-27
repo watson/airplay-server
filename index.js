19860f3340cb884cc19dea95c73164d2d8285bbc
Thomas Watson Steen <w@tson.dk>
2014-07-27 02:31:16 +0200

    Fix features bitmask

--- index.js
+++ index.js
@@ -53,19 +53,20 @@ var airplay = module.exports = function (name, options, onRequest) {
   return server;
 };
 
-var features = airplay.features = {
-  VIDEO: 0,                   // video support
-  PHOTO: 1,                   // photo support
-  VIDEO_FAIR_PLAY: 2,         // video protected with FairPlay DRM
-  VIDEO_VOLUME_CONTROL: 4,    // volume control supported for videos
-  VIDEO_HTTP_LIVE_STREAMS: 8, // http live streaming supported
-  SLIDESHOW: 16,              // slideshow supported
-  SCREEN: 64,                 // mirroring supported
-  SCREEN_ROTATE: 128,         // screen rotation supported
-  AUDIO: 256,                 // audio supported
-  AUDIO_REDUNDANT: 512,       // audio packet redundancy supported
-  FPSAP_V2_AES_GCM: 1024,     // FairPlay secure auth supported
-  PHOTO_CACHING: 2048         // photo preloading supported
+var features = {
+  VIDEO: 1,                    // video support
+  PHOTO: 2,                    // photo support
+  VIDEO_FAIR_PLAY: 4,          // video protected with FairPlay DRM
+  // VIDEO_VOLUME_CONTROL: 8,     // volume control supported for videos
+  VIDEO_HTTP_LIVE_STREAMS: 16, // http live streaming supported
+  SLIDESHOW: 32,               // slideshow supported
+  // ??: 64                       // Unknown
+  SCREEN: 128,                 // mirroring supported
+  SCREEN_ROTATE: 256,          // screen rotation supported
+  AUDIO: 512,                  // audio supported
+  AUDIO_REDUNDANT: 1024,       // audio packet redundancy supported
+  FPSAP_V2_AES_GCM: 2048,      // FairPlay secure auth supported
+  PHOTO_CACHING: 4096          // photo preloading supported
 };
 
 var allFeatures = Object.keys(features)
