7c013a8f49760a2f22964e8102030041afb66d39
Thomas Watson Steen <w@tson.dk>
2014-08-02 21:36:22 +0200

    Add example AirPlay Server

---
+++ examples/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var debug = require('debug')('airplay');
+var airplay = require('../')();
+
+airplay.on('request', function (req, res) {
+  console.log(req.method, res.url);
+  debug(req.headers);
+});
+
+airplay.listen(function () {
+  debug('AirPlay server is now listening on port %d', airplay.address().port);
+});
