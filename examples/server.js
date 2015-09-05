2bd306cb4ba025b6039013bdcf82eb825f9b4ae5
Thomas Watson Steen <w@tson.dk>
2015-09-06 01:36:04 +0200

    standard: remove trailing semicolons

--- examples/server.js
+++ examples/server.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-var airplay = require('../');
+var airplay = require('../')
 
 var server = airplay('My AirPlay Server', function (req, res) {
-  console.log(req.method, req.url);
-  console.log(req.headers);
-});
+  console.log(req.method, req.url)
+  console.log(req.headers)
+})
 
 server.listen(7000, function () {
-  console.log('AirPlay server is listening on port %d', server.address().port);
-});
+  console.log('AirPlay server is listening on port %d', server.address().port)
+})
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-'use strict';
+'use strict'
 
-var http = require('http');
-var mdns = require('airplay-mdns-server');
-var xtend = require('xtend');
-var pkg = require('./package');
-var debug = require('debug')(pkg.name);
+var http = require('http')
+var mdns = require('airplay-mdns-server')
+var xtend = require('xtend')
+var pkg = require('./package')
+var debug = require('debug')(pkg.name)
 
 var airplay = module.exports = function (opts, onRequest) {
-  if (typeof opts === 'function') return airplay(null, opts);
-  if (typeof opts === 'string') return airplay({ name: opts }, onRequest);
-  if (!opts) opts = {};
+  if (typeof opts === 'function') return airplay(null, opts)
+  if (typeof opts === 'string') return airplay({ name: opts }, onRequest)
+  if (!opts) opts = {}
 
-  var server = http.createServer(onRequest);
+  var server = http.createServer(onRequest)
 
   server.on('request', function (req, res) {
-    debug('%s %s', req.method, req.url, req.headers);
-  });
+    debug('%s %s', req.method, req.url, req.headers)
+  })
 
   server.on('listening', function () {
-    var port = server.address().port;
-    debug('Listening on port %d', port);
+    var port = server.address().port
+    debug('Listening on port %d', port)
 
-    opts = xtend({ name: 'Node.js', version: pkg.version, port: port }, opts);
+    opts = xtend({ name: 'Node.js', version: pkg.version, port: port }, opts)
 
     mdns(opts, function (err) {
-      if (err) server.emit('error', err);
-    });
-  });
+      if (err) server.emit('error', err)
+    })
+  })
 
-  return server;
-};
+  return server
+}
